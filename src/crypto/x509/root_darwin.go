a89c4f8ac8d536040da243a9239203060234cd3f
Adam Shannon <adamkshannon@gmail.com>
2018-02-26 17:38:15 -0600

    crypto/x509: parse darwin trust policies

--- src/crypto/x509/root_darwin.go
+++ src/crypto/x509/root_darwin.go
@@ -7,22 +7,26 @@
 package x509
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/sha1"
 	"encoding/pem"
+	"encoding/xml"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"os/user"
 	"path/filepath"
+	"regexp"
 	"strings"
-	"sync"
 )
 
-var debugExecDarwinRoots = strings.Contains(os.Getenv("GODEBUG"), "x509roots=1")
+var (
+	debugExecDarwinRoots = strings.Contains(os.Getenv("GODEBUG"), "x509roots=1")
+
+	// From Apple Security OSS trust/SecTrustSettings.h
+	kSecTrustSettingsResultDeny = "3"
+)
 
 func (c *Certificate) systemVerify(opts *VerifyOptions) (chains [][]*Certificate, err error) {
 	return nil, nil
@@ -40,26 +44,21 @@ func (c *Certificate) systemVerify(opts *VerifyOptions) (chains [][]*Certificate
 //
 // 1. Run "security trust-settings-export" and "security
 //    trust-settings-export -d" to discover the set of certs with some
-//    user-tweaked trust policy. We're too lazy to parse the XML (at
-//    least at this stage of Go 1.8) to understand what the trust
-//    policy actually is. We just learn that there is _some_ policy.
+//    user-tweaked trust policy. Parse that resulting xml and understand
+//    what the trust policies actually are.
 //
 // 2. Run "security find-certificate" to dump the list of system root
 //    CAs in PEM format.
 //
-// 3. For each dumped cert, conditionally verify it with "security
-//    verify-cert" if that cert was in the set discovered in Step 1.
-//    Without the Step 1 optimization, running "security verify-cert"
-//    150-200 times takes 3.5 seconds. With the optimization, the
-//    whole process takes about 180 milliseconds with 1 untrusted root
-//    CA. (Compared to 110ms in the cgo path)
+// 3. For each dumped cert, check if the certificate is marked as 'trustRoot'
+//    and if so add to our CertPool
 func execSecurityRoots() (*CertPool, error) {
-	hasPolicy, err := getCertsWithTrustPolicy()
+	certPolicies, err := getCertsWithTrustPolicy()
 	if err != nil {
 		return nil, err
 	}
 	if debugExecDarwinRoots {
-		println(fmt.Sprintf("crypto/x509: %d certs have a trust policy", len(hasPolicy)))
+		println(fmt.Sprintf("crypto/x509: %d certs have a trust policy", len(certPolicies)))
 	}
 
 	args := []string{"find-certificate", "-a", "-p",
@@ -87,53 +86,8 @@ func execSecurityRoots() (*CertPool, error) {
 		return nil, err
 	}
 
-	var (
-		mu          sync.Mutex
-		roots       = NewCertPool()
-		numVerified int // number of execs of 'security verify-cert', for debug stats
-	)
+	roots := NewCertPool()
 
-	blockCh := make(chan *pem.Block)
-	var wg sync.WaitGroup
-
-	// Using 4 goroutines to pipe into verify-cert seems to be
-	// about the best we can do. The verify-cert binary seems to
-	// just RPC to another server with coarse locking anyway, so
-	// running 16 at a time for instance doesn't help at all. Due
-	// to the "if hasPolicy" check below, though, we will rarely
-	// (or never) call verify-cert on stock macOS systems, though.
-	// The hope is that we only call verify-cert when the user has
-	// tweaked their trust policy. These 4 goroutines are only
-	// defensive in the pathological case of many trust edits.
-	for i := 0; i < 4; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for block := range blockCh {
-				cert, err := ParseCertificate(block.Bytes)
-				if err != nil {
-					continue
-				}
-				sha1CapHex := fmt.Sprintf("%X", sha1.Sum(block.Bytes))
-
-				valid := true
-				verifyChecks := 0
-				if hasPolicy[sha1CapHex] {
-					verifyChecks++
-					if !verifyCertWithSystem(block, cert) {
-						valid = false
-					}
-				}
-
-				mu.Lock()
-				numVerified += verifyChecks
-				if valid {
-					roots.AddCert(cert)
-				}
-				mu.Unlock()
-			}
-		}()
-	}
 	for len(data) > 0 {
 		var block *pem.Block
 		block, data = pem.Decode(data)
@@ -143,62 +97,52 @@ func execSecurityRoots() (*CertPool, error) {
 		if block.Type != "CERTIFICATE" || len(block.Headers) != 0 {
 			continue
 		}
-		blockCh <- block
-	}
-	close(blockCh)
-	wg.Wait()
 
-	if debugExecDarwinRoots {
-		mu.Lock()
-		defer mu.Unlock()
-		println(fmt.Sprintf("crypto/x509: ran security verify-cert %d times", numVerified))
+		cert, err := ParseCertificate(block.Bytes)
+		if err != nil {
+			continue
+		}
+		sha1CapHex := fmt.Sprintf("%X", sha1.Sum(block.Bytes))
+
+		policies, exists := certPolicies[sha1CapHex]
+		if !exists { // no user defined policy so accept the cert
+			roots.AddCert(cert)
+			continue
+		}
+		trusted := true
+		for i := range policies {
+			if policies[i].name == "sslServer" && policies[i].result == kSecTrustSettingsResultDeny {
+				trusted = false
+				if debugExecDarwinRoots {
+					println(fmt.Sprintf("crypto/x509: %s has Deny policy set", cert.Subject))
+				}
+				break
+			}
+		}
+		if trusted {
+			roots.AddCert(cert)
+		}
 	}
 
 	return roots, nil
 }
 
-func verifyCertWithSystem(block *pem.Block, cert *Certificate) bool {
-	data := pem.EncodeToMemory(block)
-
-	f, err := ioutil.TempFile("", "cert")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "can't create temporary file for cert: %v", err)
-		return false
-	}
-	defer os.Remove(f.Name())
-	if _, err := f.Write(data); err != nil {
-		fmt.Fprintf(os.Stderr, "can't write temporary file for cert: %v", err)
-		return false
-	}
-	if err := f.Close(); err != nil {
-		fmt.Fprintf(os.Stderr, "can't write temporary file for cert: %v", err)
-		return false
-	}
-	cmd := exec.Command("/usr/bin/security", "verify-cert", "-c", f.Name(), "-l", "-L")
-	var stderr bytes.Buffer
-	if debugExecDarwinRoots {
-		cmd.Stderr = &stderr
-	}
-	if err := cmd.Run(); err != nil {
-		if debugExecDarwinRoots {
-			println(fmt.Sprintf("crypto/x509: verify-cert rejected %s: %q", cert.Subject.CommonName, bytes.TrimSpace(stderr.Bytes())))
-		}
-		return false
-	}
-	if debugExecDarwinRoots {
-		println(fmt.Sprintf("crypto/x509: verify-cert approved %s", cert.Subject.CommonName))
-	}
-	return true
+type trustPolicy struct {
+	name string // kSecTrustSettingsPolicyName values (e.g. basicX509, sslServer, CodeSigning)
+	result string // kSecTrustSettingsResult
 }
 
 // getCertsWithTrustPolicy returns the set of certs that have a
-// possibly-altered trust policy. The keys of the map are capitalized
-// sha1 hex of the raw cert.
-// They are the certs that should be checked against `security
-// verify-cert` to see whether the user altered the default trust
-// settings. This code is only used for cgo-disabled builds.
-func getCertsWithTrustPolicy() (map[string]bool, error) {
-	set := map[string]bool{}
+// possibly-altered trust policy.
+//
+// The keys of the map are capitalized sha1 hex of the raw cert.
+// The values of the map are arrays of trustPolicy which hold the
+// policy name and result
+//
+// This code is only used for cgo-disabled builds.
+func getCertsWithTrustPolicy() (map[string][]*trustPolicy, error) {
+	set := map[string][]*trustPolicy{}
+
 	td, err := ioutil.TempDir("", "x509trustpolicy")
 	if err != nil {
 		return nil, err
@@ -224,36 +168,43 @@ func getCertsWithTrustPolicy() (map[string]bool, error) {
 			return nil
 		}
 
-		f, err := os.Open(file)
+		bs, err := ioutil.ReadFile(file)
 		if err != nil {
 			return err
 		}
-		defer f.Close()
 
-		// Gather all the runs of 40 capitalized hex characters.
-		br := bufio.NewReader(f)
-		var hexBuf bytes.Buffer
-		for {
-			b, err := br.ReadByte()
-			isHex := ('A' <= b && b <= 'F') || ('0' <= b && b <= '9')
-			if isHex {
-				hexBuf.WriteByte(b)
-			} else {
-				if hexBuf.Len() == 40 {
-					set[hexBuf.String()] = true
-				}
-				hexBuf.Reset()
-			}
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				return err
-			}
+		plist := plist{}
+		err = xml.Unmarshal(bs, &plist)
+		if err != nil {
+			return err
 		}
 
+		for i := range plist.Dict {
+			// Grab all SHA1 hashesn
+			// <key>02FAF3E291435468607857694DF5E45B68851868</key>
+			// <dict>
+			//   ...
+			// </dict>
+			hashes := plist.Dict[i].Dict[0].Key
+
+			// Dive down into the xml
+			for j := range plist.Dict[i].Dict {
+				for k := range plist.Dict[i].Dict[j].Dict {
+					dict := plist.Dict[i].Dict[j].Dict[k]
+
+					hash := hashes[k].Text
+					for l := range dict.Key {
+						if dict.Key[l].Text == "trustSettings" {
+							set[hash] = parseTrustSettings(dict)
+							println(fmt.Sprintf("found trustSettings for %s, %v", hash[:8], set[hash]))
+						}
+					}
+				}
+			}
+		}
 		return nil
 	}
+
 	if err := run("user", "trust-settings-export"); err != nil {
 		return nil, fmt.Errorf("dump-trust-settings (user): %v", err)
 	}
@@ -261,4 +212,72 @@ func getCertsWithTrustPolicy() (map[string]bool, error) {
 		return nil, fmt.Errorf("dump-trust-settings (admin): %v", err)
 	}
 	return set, nil
+}
+
+var (
+	nonContentRegex    = regexp.MustCompile(`[^a-zA-Z0-9\+\/=]*`)
+	whitespaceReplacer = strings.NewReplacer("\t", "", "\n", "", " ", "", "\r", "")
+)
+
+type plist struct {
+	Dict []*Dict `xml:"dict,omitempty"`
+}
+
+type Dict struct {
+	Array *Array `xml:"array,omitempty"`
+	Dict []*Dict `xml:"dict,omitempty"`
+	Integer []*Integer `xml:"integer,omitempty"`
+	Key []*Key `xml:"key,omitempty"`
+	String *String `xml:"string,omitempty"`
+}
+
+type Key struct {
+	Text string `xml:",chardata"`
+}
+
+type Array struct {
+	Dict []*Dict `xml:"dict,omitempty"`
+}
+
+type Integer struct {
+	Text string `xml:",chardata"`
+}
+
+type String struct {
+	Text string `xml:",chardata"`
+}
+
+// <key>trustSettings</key>
+// <array>
+//     <dict>
+// 	<key>kSecTrustSettingsResult</key>
+// 	<integer>4</integer>
+//     </dict>
+// </array>
+func parseTrustSettings(dict *Dict) []*trustPolicy {
+	var policies []*trustPolicy
+
+	for l := range dict.Array.Dict {
+		policy := trustPolicy{}
+		for m := range dict.Array.Dict[l].Key {
+			key := strings.TrimSpace(dict.Array.Dict[l].Key[m].Text)
+			switch key {
+			case "kSecTrustSettingsResult":
+				if len(dict.Array.Dict) >= l+1 {
+					policy.result = strings.TrimSpace(dict.Array.Dict[l].Integer[0].Text)
+				}
+
+			case "kSecTrustSettingsPolicyName":
+				if len(dict.Array.Dict) >= l+1 {
+					policy.name = strings.TrimSpace(dict.Array.Dict[l].String.Text)
+				}
+			}
+		}
+		if policy.name != "" && policy.result != "" {
+			policies = append(policies, &policy)
+		} else if debugExecDarwinRoots {
+			println(fmt.Sprintf("crypto/x509: not adding partially parsed policy %#v", policy))
+		}
+	}
+	return policies
 }
